205a6c075eaadf053b228bce0674f5848f8cd9b0
satish-gatti <satishgatti@gmail.com>
2017-06-09 11:18:30 +0530

    Update

--- app/src/main/java/net/skoolsmart/googlemap/MapsActivity.java
+++ app/src/main/java/net/skoolsmart/googlemap/MapsActivity.java
@@ -12,11 +12,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +34,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        marker=mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+            @Override
+            public void onCameraMove() {
+                // Get the center of the Map.
+                LatLng centerOfMap = mMap.getCameraPosition().target;
+
+                // Update your Marker's position to the center of the Map.
+                marker.setPosition(centerOfMap);
+            }
+        });
+
     }
+
+
 
 
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
